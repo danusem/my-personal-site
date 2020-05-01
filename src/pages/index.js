be867be160c31e253eaac9cbc64d867ccb416454
Dan Usem <danusem@gmail.com>
2020-05-01 15:00:24 -0500

    move resume and add button

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ export default () => {
             <h1>Software Engineer | Tech Enthusiast</h1>
             <p>Hi, my name is Dan Usem and I'm a software engineer based in Chicago.</p>
             <a style={{ display:"flex", textDecoration:"none" }} href="/static/DanUsemResume.pdf" download>Resume &nbsp; <img src={svg} style={{ height:"1rem" }} alt="logo"/></a>
-            <Link to="/contact">Get In Touch</Link>
+            <button><Link to="/contact">Get In Touch</Link></button>
         </Layout>
     )
 }
