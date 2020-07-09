fba9622a7eb34b2270dd1a784b27a9c37cd337ce
Dan Usem <danusem@gmail.com>
2020-07-09 15:33:27 -0500

    Sales Engineer Update

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,8 +5,8 @@ export default () => {
     return (
         <Layout>
             <h1>About Me</h1>
-            <p>Hello! I'm Dan, a software engineer based in Chicago. I just graduated from General Assembly's Software Engineering Immersive and I'm eager to start this next chapter in my career!</p>
-            <p>Beyond software I also have 8 years of experience in the tech startup world.</p>
+            <p>Hello! I'm Dan, a sales engineer based in Chicago. I just graduated from General Assembly's Software Engineering Immersive and I'm eager to start this next chapter in my career!</p>
+            <p>Beyond software I also have 8 years of client facing experience in the tech startup world.</p>
             <p>Feel free to reach out with any additional questions.</p>
         </Layout>
     )
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,9 +9,9 @@ import svg from "../images/symbol.svg"
 export default () => {
     return (
         <Layout>
-            <h1>Software Engineer | Tech Enthusiast</h1>
-            <p>Hi, my name is Dan Usem and I'm a software engineer based in Chicago.</p>
-            <a style={{ display:"flex", textDecoration:"none" }} href="DanUsemResume.pdf" download>Resume &nbsp; <img src={svg} style={{ height:"1rem" }} alt="logo"/></a>
+            <h1>Sales Engineer | Tech Enthusiast</h1>
+            <p>Hi, my name is Dan Usem and I'm a sales engineer based in Chicago.</p>
+            <a style={{ display:"flex", textDecoration:"none" }} href="Dan_Usem_Resume.pdf" download>Resume &nbsp; <img src={svg} style={{ height:"1rem" }} alt="logo"/></a>
            <Link to="/contact">Get In Touch</Link>
         </Layout>
     )
